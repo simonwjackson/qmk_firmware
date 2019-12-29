090894bc5cef694f323da653b84f4b0ef59da261
Simon W. Jackson <simon@simonwjackson.com>
2019-12-28 18:52:08 -0600

    Faster mouse

--- keyboards/keebio/levinson/keymaps/simonwjackson/config.h
+++ keyboards/keebio/levinson/keymaps/simonwjackson/config.h
@@ -56,11 +56,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AdafruitBleIRQPin   C7
 #define AdafruitBleResetPin F5
 
-// #define MK_3_SPEED
-#define MOUSEKEY_DELAY	300
+#define MK_3_SPEED
+#define MOUSEKEY_DELAY	0
 #define MOUSEKEY_INTERVAL	50
 #define MOUSEKEY_MAX_SPEED	10
-#define MOUSEKEY_TIME_TO_MAX	20
+#define MOUSEKEY_TIME_TO_MAX	0
 #define MOUSEKEY_WHEEL_MAX_SPEED	8	
 #define MOUSEKEY_WHEEL_TIME_TO_MAX	40	
 
